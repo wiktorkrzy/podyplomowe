5841f528d36791472464b34fa920b6ee5b0d1c9e
wiktorkrzy <wkrzywdz@student.wszib.edu.pl>
2023-10-08 16:00:30 +0200

    Initial commit

---
+++ src/AddNumbers.java
@@ -0,0 +1,8 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int result = a + b;
+        System.out.println("Result: " + result);
+    }
+}
